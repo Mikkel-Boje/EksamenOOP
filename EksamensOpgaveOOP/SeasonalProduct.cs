dc7403708856bf74d128b82be4fd0b6da7be5bcc
Mikkel-Boje <milars20@student.aau.dk>
2021-12-09 09:34:21 +0100

    Finished Seasonal product

--- EksamensOpgaveOOP/SeasonalProduct.cs
+++ EksamensOpgaveOOP/SeasonalProduct.cs
@@ -1,7 +1,13 @@
 using System;
 
 namespace Stregsystemet {
-    public class SeasonalProduct {
-        
+    public class SeasonalProduct : Product {
+        public SeasonalProduct( DateTime seasonStartDate, DateTime seasonEndDate, 
+                                string name, double price, bool isActive) : base(name, price, isActive) {
+            this.seasonStartDate = seasonStartDate;
+            this.seasonEndDate = seasonEndDate;
+        }
+        public DateTime seasonStartDate { get; init; }
+        public DateTime seasonEndDate { get; init; }
     }
 }
